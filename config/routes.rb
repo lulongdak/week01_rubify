a7a23fc7c778329b40e16a7cc9e3c45b574fe699
Weir Rubify <weir@rubify.com>
2017-07-05 17:20:34 +0700

    project

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  get 'welcome/index'
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
+  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
